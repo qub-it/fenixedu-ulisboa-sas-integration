f6b1294a2c7200fea00fcf5a19b2644ad2f0dabf
Gilberto Pedrosa <gilberto.pedrosa@qub-it.com>
2018-10-23 20:05:33 +0100

    improved - sicabe task

--- src/main/java/org/fenixedu/ulisboa/integration/sas/tasks/sicabe/IngestSasScholarshipSicabe.java
+++ src/main/java/org/fenixedu/ulisboa/integration/sas/tasks/sicabe/IngestSasScholarshipSicabe.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 
+import org.apache.commons.lang.exception.ExceptionUtils;
 import org.fenixedu.academic.domain.ExecutionYear;
 import org.fenixedu.academic.domain.util.email.Message;
 import org.fenixedu.academic.domain.util.email.Recipient;
@@ -15,6 +16,8 @@ import org.fenixedu.bennu.scheduler.CronTask;
 import org.fenixedu.bennu.scheduler.annotation.Task;
 import org.fenixedu.ulisboa.integration.sas.service.sicabe.SicabeExternalService;
 
+import pt.ist.fenixframework.Atomic;
+
 //Force task to be read only and process each report on its own transaction to avoid errors in a report affecting other reports
 @Task(englishTitle = "Ingest SAS Scholarships from SICABE", readOnly = true)
 public class IngestSasScholarshipSicabe extends CronTask {
@@ -22,20 +25,37 @@ public class IngestSasScholarshipSicabe extends CronTask {
     @Override
     public void runTask() throws Exception {
 
-        final int beforeSasCandidacies = Bennu.getInstance().getSasScholarshipCandidaciesSet().size();
-        final long beforeWithStateModified = getNumberOfCandidaciesWithModifiedState();
+        try {
+            final int beforeSasCandidacies = Bennu.getInstance().getSasScholarshipCandidaciesSet().size();
+            final long beforeWithStateModified = getNumberOfCandidaciesWithModifiedState();
 
-        final SicabeExternalService sicabe = new SicabeExternalService();
-        final ExecutionYear currentExecutionYear = ExecutionYear.readCurrentExecutionYear();
-        sicabe.removeAllCandidaciesWithoutRegistrationAndLoadAllSasCandidacies(currentExecutionYear);
+            final SicabeExternalService sicabe = new SicabeExternalService();
+            final ExecutionYear currentExecutionYear = ExecutionYear.readCurrentExecutionYear();
+            sicabe.removeAllCandidaciesWithoutRegistrationAndLoadAllSasCandidacies(currentExecutionYear);
 
-        sicabe.processAllSasScholarshipCandidacies(currentExecutionYear);
+            sicabe.processAllSasScholarshipCandidacies(currentExecutionYear);
 
-        final int afterSasCandidacies = Bennu.getInstance().getSasScholarshipCandidaciesSet().size();
-        final long afterWithStateModified = getNumberOfCandidaciesWithModifiedState();
+            final int afterSasCandidacies = Bennu.getInstance().getSasScholarshipCandidaciesSet().size();
+            final long afterWithStateModified = getNumberOfCandidaciesWithModifiedState();
 
-        if (beforeSasCandidacies != afterSasCandidacies || beforeWithStateModified != afterWithStateModified) {
-            sendEmailForUser();
+            if (beforeSasCandidacies != afterSasCandidacies || beforeWithStateModified != afterWithStateModified) {
+                sendEmailForUser(
+                        BundleUtil.getString(SasSpringConfiguration.BUNDLE,
+                                "sasScholarship.ingestion.task.message.notification.subject"),
+                        BundleUtil.getString(SasSpringConfiguration.BUNDLE,
+                                "sasScholarship.ingestion.task.message.notification.body",
+                                String.valueOf(afterSasCandidacies - beforeSasCandidacies),
+                                String.valueOf(afterWithStateModified - beforeWithStateModified)));
+            }
+
+        } catch (Throwable e) {
+            sendEmailForUser(
+                    BundleUtil.getString(SasSpringConfiguration.BUNDLE,
+                            "sasScholarship.ingestion.task.message.notification.subject.error"),
+                    BundleUtil.getString(SasSpringConfiguration.BUNDLE,
+                            "sasScholarship.ingestion.task.message.notification.body.error", ExceptionUtils.getStackTrace(e)));
+
+            throw e;
         }
 
     }
@@ -44,12 +64,10 @@ public class IngestSasScholarshipSicabe extends CronTask {
         return Bennu.getInstance().getSasScholarshipCandidaciesSet().stream().filter(c -> c.isModified()).count();
     }
 
-    public void sendEmailForUser() {
+    @Atomic
+    private void sendEmailForUser(final String subject, final String body) {
 
         final String emailAddress = Bennu.getInstance().getSocialServicesConfiguration().getEmail();
-
-        final String subject = BundleUtil.getString(SasSpringConfiguration.BUNDLE, "message.notification.subject");
-        final String body = BundleUtil.getString(SasSpringConfiguration.BUNDLE, "message.notification.body");
 
         new Message(Bennu.getInstance().getSystemSender(), Collections.<ReplyTo> emptyList(), Collections.<Recipient> emptyList(),
                 subject, body, new HashSet<String>(Arrays.asList(emailAddress.split(","))));
